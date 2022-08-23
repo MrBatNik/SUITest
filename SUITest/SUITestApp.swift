fc79564d30fcd193f8b5173fc4b7a5e2d05c3d14
Nikita <nicandreo@gmail.com>
2022-08-23 11:15:55 +0300

    Initial Commit

---
+++ SUITest/SUITestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SUITestApp.swift
+//  SUITest
+//
+//  Created by Nikita Batrakov on 23.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct SUITestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
